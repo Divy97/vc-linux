ab90b49c094b8f307568a89ab96082b0ba4c33ea
Divy <divyparekh1810@gmail.com>
2023-04-19 23:06:46 +0530

    fixed bugs

--- public/js/script.js
+++ public/js/script.js
@@ -114,6 +114,7 @@ const handleSubmit = async (e) => {
         const err = await response.text()
 
         messageDiv.innerHTML = "Something went wrong"
+        console.log(err);
         alert(err)
     }
 }
